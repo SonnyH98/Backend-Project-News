3fa77b15cc01619634ab249fb6196b31c8fa9335
SonnyH98 <sonnyhuggett1@gmail.com>
2022-08-05 10:23:31 +0100

    Removed console.log

--- controllers/api-info.controller.js
+++ controllers/api-info.controller.js
@@ -1,7 +1,6 @@
 const { readJSON } = require('../models/api-info.model.js');
 
 exports.sendJSON = (req, res, next) => {
-  console.log('a');
   readJSON()
     .then((apiData) => {
       res.status(200).send(apiData);
